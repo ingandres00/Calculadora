1dbb806495a579f6491f324029c2c4d245d8b13b
ingandres00 <juliochaparro8706@gmail.com>
2016-09-30 19:08:49 -0500

    Primera Versión Calculadora

---
+++ js/main.js
@@ -0,0 +1,27 @@
+var numero1 = parseInt(prompt('Ingrese su primer número',""));
+var numero2 = parseInt(prompt('Ingrese su Segundo número',""));
+var operacion = prompt("Ingrese la Operación","");
+var resultado = 0;
+
+
+
+console.log(numero1);
+console.log(numero2);
+
+
+	if (operacion=="suma" || operacion =="Suma" || operacion == "SUMA"){
+			resultado = numero1 + numero2;
+      document.write(resultado);
+		}
+  if (operacion=="resta" || operacion =="Resta" || operacion == "RESTA"){
+    resultado = numero1 - numero2;
+    document.write(resultado);
+  }
+  if (operacion=="multiplicacion" || operacion =="Multiplicacion" || operacion == "MULTIPLICACION"){
+    resultado = numero1 * numero2;
+    document.write(resultado);
+  }
+  if (operacion=="division" || operacion =="Division" || operacion == "DIVISION"){
+    resultado = numero1 / numero2;
+    document.write(resultado);
+  }
