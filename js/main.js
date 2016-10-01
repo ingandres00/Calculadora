609f0eae0af68baafe71e2c91e11d24b68662d6a
ingandres00 <juliochaparro8706@gmail.com>
2016-09-30 19:34:25 -0500

    Segunda Versión Calculadora

--- js/main.js
+++ js/main.js
@@ -1,13 +1,10 @@
 var numero1 = parseInt(prompt('Ingrese su primer número',""));
 var numero2 = parseInt(prompt('Ingrese su Segundo número',""));
-var operacion = prompt("Ingrese la Operación","");
+var operacion = prompt("Ingrese la Operación: suma, resta, multiplicacion,division, mod","");
 var resultado = 0;
-
-
 
 console.log(numero1);
 console.log(numero2);
-
 
 	if (operacion=="suma" || operacion =="Suma" || operacion == "SUMA"){
 			resultado = numero1 + numero2;
@@ -23,5 +20,9 @@ console.log(numero2);
   }
   if (operacion=="division" || operacion =="Division" || operacion == "DIVISION"){
     resultado = numero1 / numero2;
+    document.write(resultado);
+  }
+  if (operacion=="mod" || operacion =="Mod" || operacion == "MOD"){
+    resultado = numero1%numero2;
     document.write(resultado);
   }
